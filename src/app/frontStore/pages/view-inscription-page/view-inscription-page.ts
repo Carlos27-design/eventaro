5cc42d082189acbe963e341c7582b357a5557a82
Carlos27-design <coronadoc899@gmail.com>
2025-09-08 22:36:45 -0300

    generate qr code

--- src/app/frontStore/pages/view-inscription-page/view-inscription-page.ts
+++ src/app/frontStore/pages/view-inscription-page/view-inscription-page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { InscriptionService } from '../../../inscription/services/inscription-service';
+import { ActivatedRoute } from '@angular/router';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { GenerateQrInscription } from '../../../inscription/components/generate-qr-inscription/generate-qr-inscription';
 
 @Component({
   selector: 'app-view-inscription-page',
-  imports: [],
+  imports: [GenerateQrInscription],
   templateUrl: './view-inscription-page.html',
-  styleUrl: './view-inscription-page.css'
+  styleUrl: './view-inscription-page.css',
 })
 export class ViewInscriptionPage {
+  public inscriptionId = signal<string>('');
+  private readonly _inscriptionService = inject(InscriptionService);
+  private readonly _route = inject(ActivatedRoute);
 
+  constructor() {
+    this.inscriptionId.set(this._route.snapshot.params['id']);
+  }
+
+  public inscriptionResource = rxResource({
+    params: () => ({ id: this.inscriptionId() }),
+    stream: ({ params }) =>
+      this._inscriptionService.getInscriptionById(params.id),
+  });
 }
--- src/app/ubication/interfaces/ubication.ts
+++ src/app/ubication/interfaces/ubication.ts
@@ -1,6 +1,6 @@
 export interface Ubication {
   id: string;
   name: string;
-  lat: number;
-  lng: number;
+  latitude: number;
+  longitude: number;
 }
